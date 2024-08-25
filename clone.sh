30fac9ee2521e28f1001387a7bd5400abd48a321
VadimVagapov <121946131+VadimVagapov@users.noreply.github.com>
2024-08-25 17:33:00 +0500

    Add files via upload

---
+++ clone.sh
@@ -0,0 +1,8 @@
+psql -c 'create database cinema_auth_service_db;';
+psql -c 'create database cinema_comment_service_db;';
+psql -c 'create database cinema_content_service_db;';
+psql -c 'create database cinema_folder_service_db;';
+psql -c 'create database cinema_review_service_db;';
+psql -c 'create database cinema_media_service_db;';
+psql -c 'create database cinema_collection_service_db;';
+psql -c 'create database cinema_user_service_db;';
